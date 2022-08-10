b8a6a0ff517e5da31942b0c7f67d9206997b8f91
Azul Giannetto <azulgiannetto@gmail.com>
2022-08-10 12:09:45 -0300

    Change in sign

--- server/src/helpers/generateToken.js
+++ server/src/helpers/generateToken.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 const generateToken = () => {
   const token = jwt.sign(
     {
-      data: 'Azul'
+      data: 'Data is here'
     },
     process.env.SECRET,
     { expiresIn: '10d' }
