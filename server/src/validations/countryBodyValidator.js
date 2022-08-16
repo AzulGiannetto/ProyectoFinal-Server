2e08a1af3a3cc18cd8310a6de7e92d1966d76b30
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2022-08-16 14:27:20 -0300

    comment repited

--- server/src/validations/countryBodyValidator.js
+++ server/src/validations/countryBodyValidator.js
@@ -21,14 +21,14 @@ const paramsSchema = Joi.object({
   id: Joi.string().min(24).max(24).required()
 })
 
-const schema = Joi.object({
-  user: Joi.string().required().min(4).max(25),
-  // country: Joi.string().alphanum().required().trim(),
-  // continent: Joi.string().alphanum().required().trim(),
-  // hemisphere: Joi.string().alphanum().required().trim(),
-  // language: Joi.string().alphanum().required().trim(),
-  description: Joi.string().alphanum().required().max(400).min(5),
-  imageUrl: Joi.string().alphanum().required()
-})
+// const schema = Joi.object({
+//   user: Joi.string().required().min(4).max(25),
+//   // country: Joi.string().alphanum().required().trim(),
+//   // continent: Joi.string().alphanum().required().trim(),
+//   // hemisphere: Joi.string().alphanum().required().trim(),
+//   // language: Joi.string().alphanum().required().trim(),
+//   description: Joi.string().alphanum().required().max(400).min(5),
+//   imageUrl: Joi.string().alphanum().required()
+// })
 
-module.exports = { schema, countryBodySchema, countryQuerySchema, paramsSchema }
+module.exports = { countryBodySchema, countryQuerySchema, paramsSchema }
